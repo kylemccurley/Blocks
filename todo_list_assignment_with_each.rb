d8682427ed835e3bac0f0d0e563b860da8f53d0e
Kyle McCurley <kmccurley123@gmail.com>
2020-03-08 19:49:23 -0700

    Created todo_list_assignement_with_each file
    Creating a class identical to the todo_list_assignment.rb assignment. The TodoList class has the TodoList#each method invoked with the { |todo| puts todo } block made implicitly available. The each method fetches the element at the position of the passed in argument, and yields to a block, passing in that element as a block parameter.

---
+++ todo_list_assignment_with_each.rb
@@ -0,0 +1,134 @@
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+
+  def ==(otherTodo)
+    title == otherTodo.title &&
+      description == otherTodo.description &&
+      done == otherTodo.done
+  end
+end
+# This class represents a collection of Todo objects.
+# You can perform typical collection-oriented actions
+# on a TodoList object, including iteration and selection.
+
+class TodoList
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+  
+  def each
+    index = 0
+
+    while index < todos.size
+      yield todos.fetch(index)
+      index += 1
+    end
+
+    todos
+  end
+  
+  def to_s
+    todos.to_s
+  end
+  
+  def first
+    todos.first
+  end
+  
+  def to_a
+    todos
+  end
+  
+  def item_at(position)
+    todos.fetch(position)
+  end
+  
+  def mark_undone_at(position)
+    todos.fetch(position).undone!
+  end
+  
+  def mark_done_at(position)
+    todos.fetch(position).done!
+  end
+  
+  def done!
+    todos.map! { |item| item.done! }
+  end
+  
+  def done?
+    todos.all? { |item| item.done? == true }
+  end
+  
+  def last
+    todos.last
+  end
+
+  def add(item)
+    unless item.class == Todo
+      raise TypeError.new 'Only Todo objects can be added to the list'
+    else
+      todos << item
+    end
+  end
+  
+  def remove_at(pos)
+    todos.delete(pos - 1)
+  end
+  
+  def size
+    todos.size
+  end
+  
+  def pop
+    todos.pop
+  end
+  
+  def shift
+    todos.shift
+  end
+  
+  private
+
+  attr_accessor :todos
+end
+
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+
+list = TodoList.new("Today's Todos")
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
+
+list.each do |todo|
+  puts todo                   # calls Todo#to_s
+end
